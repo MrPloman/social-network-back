da7006cc5900879039c6ed596790ad2eda90c701
MrPloman <pol.plana@slashmobility.com>
2020-10-23 20:16:52 +0200

    second

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || port;
+const port = 3000;
 const www = process.env.WWW || './';
 app.use(express.static(www));
 console.log(`serving ${www}`);
