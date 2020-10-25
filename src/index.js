7f270af2bd0ea9086529275ff817fb74bc958f50
MrPloman <pol.plana@slashmobility.com>
2020-10-25 18:20:13 +0100

    database

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+const { Router } = require('express');
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
-const port = 3000;
-const www = process.env.WWW || './';
-app.use(express.static(www));
-console.log(`serving ${www}`);
-app.get('*', (req, res) => {
-    res.sendFile(`index.html`, { root: www });
-});
-app.listen(port, () => console.log(`listening on http://localhost:${port}`));
+
+//const www = process.env.WWW || './';
+//app.use(express.static(www));
+
+//config
+app.set('port', process.env.PORT || 3000)
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//routes
+app.use(require('./routes/index'))
+
+//server
+app.listen(app.get('port'), () => console.log(`listening on http://localhost:${app.get('port')}`));
